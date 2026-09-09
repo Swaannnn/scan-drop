ebeece0a9a4a3e878733677b6aa3383203448c41
Swann <swanngille@gmail.com>
2026-09-09 23:28:45 +0200

    get users from message reactions

--- src/discord.ts
+++ src/discord.ts
@@ -1,8 +1,40 @@
-import { Client, GatewayIntentBits, TextChannel } from 'discord.js'
+import { Client, GatewayIntentBits, TextChannel, User } from 'discord.js'
 
 export const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 })
+
+export async function getUsersMessageReactions(channelId: string, messageId: string): Promise<User[]> {
+    try {
+        const channel = (await client.channels.fetch(channelId)) as TextChannel
+        if (!channel) return []
+
+        const message = await channel.messages.fetch(messageId)
+        if (!message) return []
+
+        const reactions = message.reactions.cache
+
+        if (reactions.size === 0) {
+            return []
+        }
+
+        const usersMap = new Map<string, User>()
+
+        for (const reaction of reactions.values()) {
+            const fetchedUsers = await reaction.users.fetch()
+            console.log(fetchedUsers)
+            fetchedUsers.forEach((u) => {
+                console.log(u)
+                return usersMap.set(u.id, u)
+            })
+        }
+
+        return Array.from(usersMap.values())
+    } catch (error) {
+        console.error(`[${new Date().toLocaleString()}] Error fetching users message reactions :`, error)
+        return []
+    }
+}
 
 export async function getLastChapterSend(channelId: string): Promise<string | null> {
     try {
@@ -20,15 +52,19 @@ export async function getLastChapterSend(channelId: string): Promise<string | nu
     }
 }
 
-export async function sendMessage(channelId: string, userId: string, lien: string) {
+export async function sendMessage(channelId: string, link: string, userIds: string[]): Promise<void> {
     try {
-        const message = `Nouveau chapitre de disponible !\nLien : ${lien}`
+        const message = `Nouveau chapitre de disponible !\nLien : ${link}`
 
         const channel = (await client.channels.fetch(channelId)) as TextChannel
         if (channel) await channel.send(message + `\n@everyone`)
 
-        const user = await client.users.fetch(userId)
-        if (user) await user.send(message)
+        await Promise.all(
+            userIds.map(async (userId) => {
+                const user = await client.users.fetch(userId)
+                await user.send(message)
+            })
+        )
 
         console.log(`[${new Date().toLocaleString()}] Message sent successfully to Discord.`)
     } catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import 'dotenv/config'
-import { client, getLastChapterSend, sendMessage } from './discorD.js'
+import { client, getLastChapterSend, getUsersMessageReactions, sendMessage } from './discord.js'
 import { getLastChapterUrl } from './scrapper.js'
 import { MANGA } from './constants.js'
 
 const TOKEN = process.env.DISCORD_TOKEN
-const CHANNEL_ID = process.env.CHANNEL_ID
-const USER_ID = process.env.USER_ID
+const CHAPTER_CHANNEL_ID = process.env.CHAPTER_CHANNEL_ID
+const REACTIONS_CHANNEL_ID = process.env.REACTIONS_CHANNEL_ID
+const REACTION_MESSAGE_ID = process.env.REACTION_MESSAGE_ID
 
-if (!TOKEN || !CHANNEL_ID || !USER_ID) {
+if (!TOKEN || !CHAPTER_CHANNEL_ID || !REACTIONS_CHANNEL_ID || !REACTION_MESSAGE_ID) {
     process.exit(1)
 }
 
-const INTERVALLE_MS = 2 * 10 * 1000 // 10 minutes
+const INTERVALLE_MS = 60 * 10 * 1000 // 10 minutes
 
 async function searchNewChapter() {
     console.log(`[${new Date().toLocaleString()}] search new chapter...`)
@@ -19,11 +20,14 @@ async function searchNewChapter() {
     const newChapterLink = await getLastChapterUrl(MANGA.OP)
     if (!newChapterLink) return
 
-    const savedChapterLink = await getLastChapterSend(CHANNEL_ID!)
+    const savedChapterLink = await getLastChapterSend(CHAPTER_CHANNEL_ID!)
 
     if (newChapterLink !== savedChapterLink) {
         console.log(`[${new Date().toLocaleString()}] New chapter available, sending notification...`)
-        await sendMessage(CHANNEL_ID!, USER_ID!, newChapterLink)
+        const users = await getUsersMessageReactions(REACTIONS_CHANNEL_ID!, REACTION_MESSAGE_ID!)
+        const userIds = users.map((user) => user.id)
+
+        await sendMessage(CHAPTER_CHANNEL_ID!, newChapterLink, userIds)
     } else {
         console.log(`[${new Date().toLocaleString()}] No new chapter available.`)
     }
